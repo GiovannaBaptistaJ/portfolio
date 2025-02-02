7a6b58392f899e6a07fa45db32f44dc4a9a026ce
GiovannaBaptistaJ <giovannabap.junges@gmail.com>
2025-02-02 15:10:01 -0300

    Criação do menu lateral

--- js/script.js
+++ js/script.js
@@ -1,20 +1,48 @@
 const btnAlterarTema = document.querySelector("#btn-alterar-tema");
 const body = document.querySelector("body");
-const imgIcone = document.querySelector("#img-icone");
-const logoIcone = document.querySelector("#logo-icone");
+const temaIcone = document.querySelector("#img-icone");
+const giovannaIcone = document.querySelector("#logo-icone");
 
 let temTemaEscuro = body.classList.contains("dark") ? true : false;
+
+const iconMenuLateral = document.querySelector("#btn-abrir-menu");
+const menuLateral = document.querySelector("#header-menu-lateral");
+const menuLinks = document.querySelectorAll('.nav-a-lateral');
+const btnFecharMenu = document.querySelector("#btn-fechar-menu");
 
 btnAlterarTema.addEventListener("click", function() {
     if (temTemaEscuro) {
         body.classList.remove("dark");
-        imgIcone.src = "./recursos/night-icone.svg";
-        logoIcone.src = "./recursos/marca-day.svg";
+        temaIcone.src = "./recursos/icone-tema-dia.svg";
+        giovannaIcone.src = "./recursos/Giovanna-dia.svg";
+        iconMenuLateral.src = "./recursos/menu-icone-dia.svg"
         temTemaEscuro = false;
     } else {
         body.classList.add("dark");
-        imgIcone.src = "./recursos/day-icone.svg";
-        logoIcone.src = "./recursos/marca-night.svg";
+        temaIcone.src = "./recursos/icone-tema-noite.svg";
+        giovannaIcone.src = "./recursos/Giovanna-noite.svg";
+        iconMenuLateral.src = "./recursos/menu-icone-noite.svg"
         temTemaEscuro = true;
     }
 });
+// Criando um evento de click para abrir o menu e criando um evento para quando o usuário clicar fora do menu lateral
+iconMenuLateral.addEventListener('click', function () {
+    alternarMenu()
+    console.log("teste");
+  });
+
+function alternarMenu() {
+    if (menuLateral.classList.contains("aberto")) {
+      menuLateral.classList.remove("aberto");
+      menuLateral.classList.add("fechado");
+      iconMenuLateral.classList.remove("oculto");
+    } else {
+      menuLateral.classList.remove("fechado");
+      menuLateral.classList.add("aberto");
+      iconMenuLateral.classList.add("oculto");
+    }
+  }
+  btnFecharMenu.addEventListener('click', () => {
+    alternarMenu()
+  });
+
