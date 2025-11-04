52e98358b081d14834588691415232fefe399e97
GiovannaBaptistaJ <giovannabap.junges@gmail.com>
2025-11-03 22:25:19 -0300

    fazendo algumas alterações

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const btnAlterarTema = document.querySelector("#btn-alterar-tema");
 const body = document.querySelector("body");
 const temaIcone = document.querySelector("#img-icone");
-const giovannaIcone = document.querySelector("#logo-icone");
+// const giovannaIcone = document.querySelector("#logo-icone");
 
 let temTemaEscuro = body.classList.contains("dark") ? true : false;
 
@@ -17,13 +17,13 @@ btnAlterarTema.addEventListener("click", function() {
     if (temTemaEscuro) {
         body.classList.remove("dark");
         temaIcone.src = "./recursos/icone-tema-dia.svg";
-        giovannaIcone.src = "./recursos/Giovanna-dia.svg";
+        // giovannaIcone.src = "./recursos/Giovanna-dia.svg";
         iconMenuLateral.src = "./recursos/menu-icone-dia.svg";
         temTemaEscuro = false;
     } else {
         body.classList.add("dark");
         temaIcone.src = "./recursos/icone-tema-noite.svg";
-        giovannaIcone.src = "./recursos/Giovanna-noite.svg";
+        // giovannaIcone.src = "./recursos/Giovanna-dia.svg";
         iconMenuLateral.src = "./recursos/menu-icone-noite.svg";
         temTemaEscuro = true;
     }
